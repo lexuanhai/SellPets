86f364850524921a854dab81b3e653b91bc80f1c
lexuanhai <emcuahai@gmail.com>
2022-10-07 07:38:32 +0700

    update

---
+++ demo/TECH/TECH/Data/DatabaseEntity/Customers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TECH.SharedKernel;
+
+namespace TECH.Data.DatabaseEntity
+{
+    [Table("Customers")]
+   public class Customers: DomainEntity<int>
+    {        
+        public string Name { get; set; }
+        [Column(TypeName ="varchar(12)")]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        [Column(TypeName = "varchar(250)")]
+        public string MatKhau { get; set; }
+        public string TenDangNhap { get; set; }
+        public string GioiTinh { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ demo/TECH/TECH/Data/DatabaseEntity/Products.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TECH.SharedKernel;
+
+namespace TECH.Data.DatabaseEntity
+{
+    [Table("products")]
+    public class Products : DomainEntity<int>
+    {
+        [Column(TypeName = "nvarchar(250)")]
+        public string? name { get; set; }
+
+        public int? category_id { get; set; }
+        [ForeignKey("category_id")]
+        public Category? Category { get; set; }
+
+
+        [Column(TypeName = "varchar(200)")]
+        public string? slug { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string? avatar { get; set; }
+
+        public decimal price { get; set; }
+
+        public string? color { get; set; }
+
+        public int quantity { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? short_desc { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? description { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? specifications { get; set; }
+
+        [Column(TypeName = "varchar(250)")]
+        public string? endow { get; set; }
+        
+        public int? status { get; set; }
+
+        public int? type { get; set; }
+
+        public int? differentiate { get; set; }
+    }
+}
